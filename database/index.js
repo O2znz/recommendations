95d1f393b294792f1464ce6cff89dfaa151230b5
Parteek S. Sandhu <ParteekSSandhu@MacBook-Pro-5.local>
2019-10-11 15:17:32 -0700

    Make schema and insert one record into db

--- database/index.js
+++ database/index.js
@@ -8,6 +8,26 @@ db.once('open', () => {
   console.log('Connected to DB!');
 });
 
-// var recommendationSchema = mongoose.Schema({});
+var recommendationSchema = mongoose.Schema({
+  id: Number,
+  name: String,
+  location: String,
+  images: [String],
+  price: String,
+  type: String,
+  rating: Number
+});
 
-// var RecommendationModel = mongoose.model('Recommendation', recommendationSchema);
+var RecommendationModel = mongoose.model('Recommendation', recommendationSchema);
+
+var recommendation = {
+  id: 0,
+  name: 'Mi Casa',
+  location: 'No se',
+  images: ['url1', 'url2', 'url3'],
+  price: 'Mucho dinero',
+  type: 'Um cama',
+  rating: 5
+};
+
+RecommendationModel.insertMany(recommendation);
