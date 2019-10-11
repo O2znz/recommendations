c303a34f3777636cfe73deb167b11547ec39e96e
Parteek S. Sandhu <ParteekSSandhu@MacBook-Pro-5.local>
2019-10-11 11:30:28 -0700

    Setup project repo with server, eslint, react, and jest

---
+++ server/index.js
@@ -0,0 +1,16 @@
+// require express and bodyParser
+const express = require('express');
+const bodyParser = require('body-parser');
+const path = require('path');
+
+const app = express();
+
+const PORT = 3004;
+
+app.use(express.static(path.join(__dirname, '../client/dist/')));
+
+app.use(bodyParser.json());
+
+app.listen(PORT, () => {
+  console.log(`Express server listening on port ${PORT}!`);
+});
